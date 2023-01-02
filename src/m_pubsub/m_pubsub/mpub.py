9a3c8f3a9fad40014b0eae24a6c2b3c67662a3ec
oYJo7 <ooyb0117@gmail.com>
2023-01-02 17:40:48 +0900

    mpub.py 파일 수정

--- src/m_pubsub/m_pubsub/mpub.py
+++ src/m_pubsub/m_pubsub/mpub.py
@@ -7,14 +7,23 @@ class M_pub(Node):
         super().__init__('message_publisher')
         self.message_publisher = self.create_publisher(String, 'm_pub', 10)
         self.timer = self.create_timer(1, self.m_publisher)
+        self.count = 0
     
-    def m_publisher(self):
-        self.message_publisher.publish()
+    def m_publisher(self, msg):
+        msg.data = 'hellow' + self.count
+        self.message_publisher.publish(msg)
+        self.count += 1
 
 def main(args=None):
     rclpy.init(args=args)
     node = M_pub()
-    rclpy.spin(node)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('Keyboard interrupt!!!!')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown
 
 # main일때만 코드 실행
 if __name__=='__main':
