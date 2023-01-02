bf2b48ef9ecdbda7add0cf7ed3ee63f3173ad307
oYJo7 <ooyb0117@gmail.com>
2023-01-02 17:35:44 +0900

    mpub.py 파일 수정

--- src/m_pubsub/m_pubsub/mpub.py
+++ src/m_pubsub/m_pubsub/mpub.py
@@ -6,9 +6,15 @@ class M_pub(Node):
     def __init__(self):
         super().__init__('message_publisher')
         self.message_publisher = self.create_publisher(String, 'm_pub', 10)
+        self.timer = self.create_timer(1, self.m_publisher)
+    
+    def m_publisher(self):
+        self.message_publisher.publish()
 
 def main(args=None):
-    pass
+    rclpy.init(args=args)
+    node = M_pub()
+    rclpy.spin(node)
 
 # main일때만 코드 실행
 if __name__=='__main':
